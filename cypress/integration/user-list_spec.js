feb952742293d2634d7bd2d0d2570ae2ff95ce25
Igor Motyka <igor_motyka@mail.com>
2021-09-23 20:40:28 +0200

    Better grouping of tests

---
+++ cypress/integration/user-list_spec.js
@@ -0,0 +1,69 @@
+import users from "../fixtures/users.json";
+
+describe("Search box", () => {
+  it("Is disabled while fetching users", () => {
+    cy.intercept("GET", "/users*", { fixture: "users" }).as("getUsers");
+    cy.visit("/");
+    cy.get("[data-cy=searchBox]").should("be.disabled");
+  });
+
+  it("Have a placeholder", () => {
+    cy.visit("/");
+    cy.get("[data-cy=searchBox]").should("have.attr", "placeholder");
+  });
+
+  it("Is focused after fetching", () => {
+    cy.intercept("GET", "/users*", { fixture: "users" }).as("getUsers");
+    cy.visit("/");
+    cy.wait(["@getUsers"]);
+    cy.get("[data-cy=searchBox]").should("be.focused");
+  });
+});
+
+describe("Fetching", () => {
+  it("Spinner is shown while fetching", () => {
+    cy.intercept("GET", "/users*", { fixture: "users" }).as("getUsers");
+    cy.visit("/");
+    cy.get("[data-cy=spinner]").should("be.visible");
+    cy.get("[data-cy=users-list]").should("not.exist");
+    cy.wait(["@getUsers"]);
+    cy.get("[data-cy=spinner]").should("not.exist");
+    cy.get("[data-cy=users-list]").should("be.visible");
+  });
+
+  it("Error is shown when fetching fails", () => {
+    cy.intercept("GET", "/users*", { forceNetworkError: true }).as("getUsers");
+    cy.visit("/");
+    cy.wait(["@getUsers"]);
+    cy.get("[data-cy=error]").should("be.visible");
+  });
+});
+
+describe("Users list", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "/users*", { fixture: "users" }).as("getUsers");
+    cy.visit("/");
+    cy.wait(["@getUsers"]);
+  });
+
+  it("Is filtered correctly", () => {
+    cy.get("input[name=search-input]").type("jAc    ");
+    cy.get("[data-cy=users-list]").contains(users[0].name);
+    cy.get("[data-cy=users-list]").contains(users[1].name);
+    cy.get("[data-cy=users-list]").children().should("have.length", 2);
+  });
+
+  it("Shows info when no user was matched", () => {
+    cy.get("input[name=search-input]").type("bleblebleblebleble");
+    cy.get("[data-cy=info]").should("be.visible");
+    cy.get("input[name=search-input]").clear();
+    cy.get("[data-cy=users-list]").should("be.visible");
+    cy.get("[data-cy=users-list]").children().should("have.length", 4);
+    cy.get("[data-cy=info]").should("not.exist");
+  });
+
+  it("Shows neccessary user info", () => {
+    cy.get("[data-cy=user-info]").first().contains(users[0].name);
+    cy.get("[data-cy=user-info]").first().contains(`@${users[0].username}`);
+  });
+});
